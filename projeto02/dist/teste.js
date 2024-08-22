df637fc170c7679553e6f0b21e399c6292383f9c
mariaelima73 <maria.e.lima73@aluno.senai.br>
2024-08-22 11:02:05 -0300

    dia 2

---
+++ projeto02/dist/teste.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome2 = "Elaine";
+let idade2 = 25;
+console.log(`Meu nome é ${nome2} e eu tenho ${idade2} anos.`);
---
+++ projeto02/src/teste.ts
@@ -0,0 +1,3 @@
+let nome2 = "Elaine";
+let idade2 = 25;
+console.log(`Meu nome é ${nome2} e eu tenho ${idade2} anos.`);
